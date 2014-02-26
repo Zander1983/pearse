504b13c8943c355161baac77c9bf28830d6b5f89
web-intelligence <info@webintelligence.ie>
2014-02-26 13:02:04 +0000

    message

--- js/app/views/VideoItem.js
+++ js/app/views/VideoItem.js
@@ -6,7 +6,8 @@ define(function (require) {
         Backbone            = require('backbone'),
         UsefulFuncs         = require('app/utils/useful_func'),
         tpl                 = require('text!tpl/VideoItem.html'),
-        template = _.template(tpl);
+        template = _.template(tpl),
+        key;
 
     return Backbone.View.extend({
 
@@ -43,48 +44,19 @@ define(function (require) {
 
         getYouTubeLink: function() {
     
-            alert('opening links...');
-    
-            if (window.device.platform == 'android' || window.device.platform == 'Android') {
-                //Android ONLY - ios can you inAppBrowser
-                navigator.app.loadUrl('https://www.youtube.com/watch?feature=player_embedded&v=1KRdf8Aif7M', { openExternal:true });
+            key = $(this.model.attributes.description).find('object').attr('data');
             
-            }
-            else{
-                window.open('https://www.youtube.com/watch?feature=player_embedded&v=1KRdf8Aif7M', '_blank');
-            }
+            var start = key.indexOf('/v/');
+            var end = key.indexOf('?');
             
-            
-            ////www.youtube-nocookie.com/v/1KRdf8Aif7M?version=3&hl=en_GB 
-            //this.model.attributes.description
-            var url = $(this.model.attributes.description).find('object').attr('data');
-            console.log('url is ');
-            console.log(url);
-            
-            var start = url.indexOf('/v/');
-            var end = url.indexOf('?');
-            
-            url = url.substring(start+3, end);
+            key = key.substring(start+3, end);
 
-            
-            //var isYouTube = RegExp(/\.youtube\.com.+v=([\w_\-]+)/i);
-            //var r = isYouTube.exec(url);
-            //if (r && r[1]) {
-              var video = 'http://www.youtube.com/v/' + url + '&hl=en&fs=1&';
-              var youtube =  '<embed src="' + video + '" type="application/x-shockwave-flash"' + 
-                ' allowscriptaccess="always"' + 
-                ' allowfullscreen="true" width="90" height="60"></embed>';
-           
-            //}
-            
-            console.log('youtube is ');
-            console.log(youtube);
-            
-            this.model.set({description: youtube});
+            console.log('key is ');
+            console.log(key);
         },
 
         render: function () {
-            this.$el.html(template({model:this.model.attributes}));
+            this.$el.html(template({model:this.model.attributes, key:key, platform:window.device.platform}));
             return this;
         },
         
